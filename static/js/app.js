8dd64343a57af64c5614b31afea3889e986cad07
Danielle Anderson <dganderson94@gmail.com>
2023-02-23 18:38:29 -0600

    more or less feature complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,10 @@
-let myMap = L.map("bubble");
+let myMap = L.map("bubble"); // TODO: change div id
 let stateData = {};
-const defaultChoice = "ALL LOCATIONS";
-let currentLocation = defaultChoice;
+const defaultChoice = "ALL STATES"; // TODO: match caps
+let prevState = defaultChoice;
 const dropdown = d3.select("#selDataset");
 const metadataPanel = d3.select("#sample-metadata");
-const fieldsToAverage = ["latitude", "longitude"];
+const fieldsToAverage = ["Latitude", "Longitude"]; // TODO: match JSON
   
 // Adding the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -25,7 +25,7 @@ const url = baseURL + date + complaint + limit;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // query API, perform initial setup
-d3.json(url).then(function(response) {
+d3.json(url).then(function(response) { // TODO: change to fetch(url)
     newState(defaultChoice);
 
     console.log(response[0]); // debug
@@ -34,7 +34,7 @@ d3.json(url).then(function(response) {
     for (const report of response) {
         let state = report.borough; // TODO: match JSON
   
-        // If a location is given for the report, put the report in the corresponding markerClusterGroup and bind a popup
+        // Only mark reports with useful info
         if (report.location && report.descriptor) { // TODO: match JSON
             if (!stateData[state]) newState(state);
             addMarker(report); // TODO: match JSON
@@ -47,18 +47,14 @@ d3.json(url).then(function(response) {
 
     // Give each location chart data and dropdown entry, calculate averages
     for (const state of stateList) {
-        // Bar chart
         stateData[state].bar[0].x = Object.keys(stateData[state].seasonCounts);
         stateData[state].bar[0].y = Object.values(stateData[state].seasonCounts);
 
-        // Line chart
         stateData[state].line[0].x = Object.keys(stateData[state].yearCounts).sort();
         stateData[state].line[0].y = stateData[state].line[0].x.map(index => stateData[state].yearCounts[index]);
 
-        // Dropdown
         dropdown.append("option").attr("value", state).text(state);
 
-        // Averages
         for (const field of fieldsToAverage) {
             metadataAverage(state, field);
         }
@@ -70,22 +66,20 @@ d3.json(url).then(function(response) {
 
 // Get average of recorded total
 function metadataAverage(state, field) {
-    let avgFieldName = "average" + field;
-    let totalFieldName = "total" + field;
-    stateData[state].metadata[avgFieldName] = stateData[state][totalFieldName] / stateData[state].metadata.totalSightings;
+    let avgFieldName = "Average " + field;
+    stateData[state].metadata[avgFieldName] = Math.round(stateData[state].totals[field] / stateData[state].counts[field] * 100) / 100;
 }
 
 // Add given value to total for given field
 function accumulateValue(state, field, addend) {
-    let totalFieldName = "total" + field;
-    stateData[state][totalFieldName] += addend;
+    stateData[state].totals[field] += addend;
+    stateData[state].counts[field]++;
 }
 
 // Add new marker
 function addMarker(report, state = defaultChoice) {
     let location = report.location; // TODO: match JSON
     let desc = report.descriptor; // TODO: match JSON
-
     let year = report.created_date.substr(5,2); // TODO: match JSON
     let season = report.descriptor; // TODO: match JSON
 
@@ -96,10 +90,10 @@ function addMarker(report, state = defaultChoice) {
     // Count sighting in relevant fields
     countSighting(state, "yearCounts", year);
     countSighting(state, "seasonCounts", season);
-    stateData[state].metadata.totalSightings++;
+    stateData[state].metadata["Total Sightings"]++;
 
     for (const field of fieldsToAverage) {
-        accumulateValue(state, field, Number(report[field]));
+        accumulateValue(state, field, Number(report[field.toLowerCase()])); // TODO: remove .toLowercase() if needed
     }
 
     if (state === defaultChoice) addMarker(report, report.borough); // Woo recursion! // TODO: match JSON
@@ -122,8 +116,10 @@ function newState(state) {
             "Mouse Sighting": 0,
             "Signs of Rodents": 0
         },
+        totals: {},
+        counts: {},
         metadata: {
-            totalSightings: 0
+            ["Total Sightings"]: 0
         },
         bar: [{
             type: "bar",
@@ -137,17 +133,17 @@ function newState(state) {
     }
 
     for (const field of fieldsToAverage) {
-        stateData[state]["total" + field] = 0;
+        stateData[state].totals[field] = 0;
+        stateData[state].counts[field] = 0;
     }
 }
 
-// Update graphics on dropdown selection
+// Update page on dropdown selection
 function optionChanged(state) {
     // Clear old stuff
     metadataPanel.selectAll("div").remove();
-    myMap.removeLayer(stateData[currentLocation].markers);
-
-    currentLocation = state;
+    myMap.removeLayer(stateData[prevState].markers);
+    prevState = state;
 
     console.log(`${state}:`); // debug
     console.log(stateData[state]); // debug
@@ -159,7 +155,7 @@ function optionChanged(state) {
     // Bar chart
     let barLayout = {
         title: {
-            text: `Bigfoot Sightings Per Season<br>In ${state}`,
+            text: `Bigfoot Sightings Per<br>Season In ${state}`,
             font: {
                 size: 20
             }
@@ -175,12 +171,12 @@ function optionChanged(state) {
             }
         }
     }
-    Plotly.newPlot("bar", stateData[state].bar, barLayout);
+    Plotly.newPlot("bar", stateData[state].bar, barLayout); // TODO: change div id
 
     // Line chart
     let lineLayout = {
         title: {
-            text: `Bigfoot Sightings Over Time<br>In ${state}`,
+            text: `Bigfoot Sightings Over<br>Time In ${state}`,
             font: {
                 size: 20
             }
@@ -196,11 +192,10 @@ function optionChanged(state) {
             }
         }
     }
-    Plotly.newPlot("gauge", stateData[state].line, lineLayout);
+    Plotly.newPlot("gauge", stateData[state].line, lineLayout); // TODO: change div id
 
     // Metadata
     for (const [key, value] of Object.entries(stateData[state].metadata)) {
         if (value) metadataPanel.append("div").text(`${key}: ${value}`);
     }
-    // TODO: styling
 }
